eb6e8713fd424e66b6b3bea579a677d38eb6bb88
Humbertzhang <504490160@qq.com>
2016-12-23 13:15:59 +0800

    Add files via upload

---
+++ map_reduce_ex2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from functools import reduce
+
+def prod(L):
+
+  for num in L:
+    return  reduce (X,L)
+
+def X(a,b):
+  return a*b
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
