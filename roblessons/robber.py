2787b737bd5c3d4e37ece7f887ad614366ef02e5
Humbertzhang <504490160@qq.com>
2018-01-04 13:58:50 +0800

    rob lessons

--- roblessons/robber.py
+++ roblessons/robber.py
@@ -10,7 +10,7 @@ WAIT_TIME = 1
 UID = "X"
 PSWD = "X"
 # MOD = 1 为按课程号选课 MOD = 2 为选通核 MOD = 3 为选通选
-MOD = 2
+MOD = 1
 
 def init():
     browser.get("https://account.ccnu.edu.cn/cas/login?service=http%3A%2F%2Fxk.ccnu.edu.cn%2Fssoserver%2Flogin%3Fywxt%3Djw%26url%3Dxtgl%2Findex_initMenu.html")
@@ -25,8 +25,10 @@ def init():
 #按课程号选课
 def choose_by_id():
     #想选的ID
-    ids = ""
-    pass
+    ids = "40152005"
+    browser.find_element_by_xpath('//*[@id="searchBox"]/div/div[2]/div/div/div/div/a/span').click()
+    browser.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/div[1]/div/div/div/div/input').send_keys(ids)
+    choose_loop()
 
 #通核
 def choose_th():
@@ -36,15 +38,22 @@ def choose_th():
 
 #通选
 def choose_tx():
-    pass
+    browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div/div[3]/div[6]/div/div[1]/ul/li[6]/a")
+    choose_loop()
 
 def choose_loop():
     while True:
         time.sleep(WAIT_TIME)
+        #查询
         browser.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div/div[1]/div/div/div/div/span/button[1]").click()
         #点击选课后判断是否选到了
+        browser.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/div[2]/div[1]/div[2]/table/tbody/tr/td[21]/button").click()
         #选到了之后print一条信息之后，关闭对话框，继续再抢
         #没选到就直接关闭对话框
+        try:
+            browser.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/button').click()
+        except:
+            print("选到了")
 
 if __name__ == '__main__':
     init()
